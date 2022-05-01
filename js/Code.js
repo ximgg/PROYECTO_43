47e047f52f4a4a78dcf9e80934cf875dffae0b1d
ximgg <90427543+ximgg@users.noreply.github.com>
2022-05-01 14:36:00 -0500

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+    fill("white")
+    textSize(15)
+    text("O N F T I N U C",700, 150)
+    fill("pink")
+    text("Hint: Performsa particular task!!",700,170)
+
+    fill("white")
+    textSize(15)
+    text("A T A D E S A B",400, 290)
+    fill("skyblue")
+    text("Hint: Stores all information!!",400,320)
+
+}
